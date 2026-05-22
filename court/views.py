8036594b8d82874750ec05584d0a9a9dcab2308f
Alisa Hoys <hoys@protonmail.com>
2026-05-22 15:05:56 +0200

    Add RegisterView logic

--- court/views.py
+++ court/views.py
@@ -8,3 +8,13 @@ from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
 from .models import CustomUser, Opinion, Argument, Tag
 from .forms import RegisterForm, OpinionForm, ArgumentForm, TagForm
+
+
+class RegisterView(FormView):
+    template_name = "court/register.html"
+    form_class = RegisterForm
+    success_url = reverse_lazy("login") #redirect path to login page after registration
+
+    def form_valid(self, form): #calls automatically when the form passes validation
+        form.save()
+        return super().form_valid(form) #does the redirect to success_url automatically.
