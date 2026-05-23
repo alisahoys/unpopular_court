0157c669b68c5592e1039d17f4f6547bf6fb8b01
Alisa Hoys <hoys@protonmail.com>
2026-05-23 11:55:52 +0200

    Add template files structure

--- unpopular_court/settings.py
+++ unpopular_court/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'unpopular_court.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'court/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
