f2b5cc52c6878609191443defed7caed79e21c43
Alisa Hoys <hoys@protonmail.com>
2026-05-25 16:31:33 +0200

    Add 'UTC' label to opinion_detail.html

--- unpopular_court/settings.py
+++ unpopular_court/settings.py
@@ -30,9 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'court',
     'allauth',
     'allauth.account',
-    'court',
 ]
 
 MIDDLEWARE = [
