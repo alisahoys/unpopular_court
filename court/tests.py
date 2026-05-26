ce002a9db1a974beafed73973eb268c9a693e222
Alisa Hoys <hoys@protonmail.com>
2026-05-26 11:15:28 +0200

    Add ProfileViewTest

--- court/tests.py
+++ court/tests.py
@@ -589,3 +589,51 @@ class ArgumentDeleteViewTest(TestCase):
         )
 
 
+class ProfileViewTest(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="testuser",
+            password="testpass123",
+            bio="My bio"
+        )
+
+    def test_profile_requires_login(self):
+        response = self.client.get(
+            reverse(
+                "profile",
+                kwargs={"username": "testuser"}
+            )
+        )
+        self.assertRedirects(
+            response,
+            "/accounts/login/?next=/profile/testuser/"
+        )
+
+    def test_profile_accessible_when_logged_in(self):
+        self.client.login(
+            username="testuser",
+            password="testpass123"
+        )
+        response = self.client.get(
+            reverse(
+                "profile",
+                kwargs={"username": "testuser"}
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_shows_bio(self):
+        self.client.login(
+            username="testuser",
+            password="testpass123"
+        )
+        response = self.client.get(
+            reverse(
+                "profile",
+                kwargs={"username": "testuser"}
+            )
+        )
+        self.assertContains(response, "My bio")
+
+
